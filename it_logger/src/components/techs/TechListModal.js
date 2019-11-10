3562944264cd24fe2e4f8eaf95f519040140e1e6
ds571 <dsandoval571@gmail.com>
2019-11-09 19:35:13 -0700

    Finished last project

--- it_logger/src/components/techs/TechListModal.js
+++ it_logger/src/components/techs/TechListModal.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import TechItem from './TechItem';
+import { getTechs } from '../../actions/techActions'
 
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+  //const [techs, setTechs] = useState([]); // using app level state and passing them in as props instead
+  //const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getTechs();
     // eslint-disable-next-line
   }, []);
 
+  /*
   const getTechs = async () => {
     setLoading(true);
     const res = await fetch("/techs");
@@ -17,14 +21,14 @@ const TechListModal = () => {
 
     setTechs(data);
     setLoading(false);
-  };
+  }; */ // no longer need this because we are calling this from the actions
 
   return (
     <div id='tech-list-modal' className='modal'>
         <div className='modal-content'>
             <h4>Technician List</h4>
             <ul className='collection'>
-                {!loading && techs.map(tech => (
+                {!loading && techs!== null && techs.map(tech => (
                     <TechItem tech={tech} key={tech.id} />
                 ))}
             </ul>
@@ -33,4 +37,13 @@ const TechListModal = () => {
   );
 };
 
-export default TechListModal;
+TechListModal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  tech: state.tech
+});
+
+export default connect(mapStateToProps, { getTechs })(TechListModal);
