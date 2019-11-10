31a76169f05810b6d9db3b312eedbac60a11e202
ds571 <dsandoval571@gmail.com>
2019-11-09 19:36:06 -0700

    added ignored log components

--- it_logger/src/components/logsnotignored/Logs.js
+++ it_logger/src/components/logsnotignored/Logs.js
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+  // const [logs, setLogs] = useState([]); comes from app level state
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
+  /* doing this within the action now instead of component
   const getLogs = async () => {
     setLoading(true);
     const res = await fetch("/logs");
@@ -18,9 +22,9 @@ const Logs = () => {
 
     setLogs(data);
     setLoading(false);
-  };
+  };*/
 
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />
   }
 
@@ -38,4 +42,13 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
+};
+
+// if you want to get anything from app level state and bring into component, bring in as prop
+const mapStateToProps = state => ({
+  log: state.log
+});
+export default connect(mapStateToProps, { getLogs })(Logs);
